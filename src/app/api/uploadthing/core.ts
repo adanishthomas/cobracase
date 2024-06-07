587052fb701b4b6667d2cb8581dad6b2c1bcd142
Danish Thomas <a.danishthomas@gmail.com>
2024-06-07 18:11:46 +0530

    added database and the second phase

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,9 +1,10 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { z } from "zod";
-const f = createUploadthing();
+import sharp from "sharp";
+import { db } from "@/db";
 
-const auth = (req: Request) => ({ id: "fakeId" });
+const f = createUploadthing();
 
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
@@ -13,8 +14,32 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       const { configId } = metadata.input;
+      const res = await fetch(file.url);
+      const buffer = await res.arrayBuffer();
 
-      return { configId };
+      const imgMetadata = await sharp(buffer).metadata();
+      const { width, height } = imgMetadata;
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        });
+        return { configId: configuration.id };
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        });
+        return { configId: updatedConfiguration.id };
+      }
     }),
 } satisfies FileRouter;
 
